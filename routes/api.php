d221e7150fe04b7b7f4ec1ab34f61a430aeb4965
Rizkyramadhan02 <ikyramadhan85@gmail.com>
2022-06-19 14:39:30 +0700

    Commit Api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('', 'App\Http\Controllers\Api\CobaController@index');
+Route::apiResources([
+    'friends' => 'App\Http\Controllers\Api\CobaController',
+    'groups' => 'App\Http\Controllers\Api\GroupsController',
+]);
